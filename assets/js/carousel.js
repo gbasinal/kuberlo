c144d56d3ab40aaab345fd966e78d0f4cd4b1fbe
gierMann <thisisgie.21@gmail.com>
2020-07-06 12:37:56 +0700

    updatE: added scroll functionality for slider category

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -13,7 +13,8 @@
 	};
 
 	Carousel.prototype.sliderCategoriesSettings = function(){
-		console.log("slider categories working");
+		// This is the settings for the slider of categories
+	
 		$('.categories-carousel').owlCarousel({
 			loop:false,
 			margin:15,
@@ -21,6 +22,47 @@
 			items: 4,
 			stagePadding: 30
 		})
+
+
+
+		// This is for the additional functionality of categories slider when user is scrolling
+
+		var lastScrollTop = 0;
+		var headerHeight = $(".header-container").outerHeight();
+		var catSliderHeight = $(".categories-slider-container").outerHeight();
+		var totalHt = catSliderHeight + headerHeight + 10;
+		var scrolledUpAlready = false;
+		var scrolledDownAlready = false;
+		$(window).scroll(function(event){
+		   var st = $(this).scrollTop();
+		   
+		   if (st > lastScrollTop){
+				  
+			// This is a flag to make sure the script below wont run again unless user scrolled up
+				if(!scrolledDownAlready){
+					TweenMax.to('.categories-slider-container' , .5 , {y : -totalHt, ease: "ease.easeOut", onComplete: function(){
+						$(".categories-slider-container").hide();
+					}})
+					scrolledDownAlready = true;
+					scrolledUpAlready = false;
+				}
+
+
+		   } else {
+
+			// This is a flag to make sure the script below wont run again unless user scrolled down
+				if(!scrolledUpAlready){
+					TweenMax.to('.categories-slider-container' , .5 , {y : 0, ease: "ease.easeIn", onStart: function(){
+						$(".categories-slider-container").show();
+					}})
+					scrolledUpAlready = true;
+					scrolledDownAlready = false;
+				}
+
+
+		   }
+		   lastScrollTop = st;
+		});		
 	}
 
 	app.Carousel = Carousel;
